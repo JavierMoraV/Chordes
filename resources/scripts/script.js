c0c1479eceba1a52108ca3461689f6c313749876
javierMora <javier.mora.vergara@ciisa.cl>
2022-07-10 23:29:08 -0400

    actualización solemne 3

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,6 +1,11 @@
-
+//alert("hoola")
 $( document ).ready(function() {
     obtenerDatos()
+    $("#sa2").on('click', function(){
+        Swal.fire('instrumentos')
+    })
+    validarDatos();
+    abrirMapa();
 });
 function obtenerDatos() {
     $.ajax('https://62b11514196a9e98702f2c86.mockapi.io/chordes', {
@@ -15,7 +20,7 @@ function obtenerDatos() {
                                 +  '<td class="prc-25">'+artistas+'</td>'
                                 +   '<td class="prc-25">'+historia+'</td>'
                                 +'</tr>'
-                console.log(filaTAbla)
+                
                 $("#tabla").append(filaTAbla)
 
             }
@@ -24,4 +29,63 @@ function obtenerDatos() {
             alert("No me pude conectar a la api: error"+errorMessage+" estado:"+textStatus  )
         }
     });
+}
+function validarDatos() {
+    nombre = document.getElementById("nombreForm").value
+    apellido = document.getElementById("apellidoForm").value
+    edad = document.getElementById("edadForm").value
+    direccion = document.getElementById("direccionForm").value
+    //otroDato = prompt("ingrese cualquier cosa")
+    //console.log("nombre: "+nombre+"\n apellido: "+apellido+ "\n edad: "+edad+ "\n direccion: "+direccion+"\n otro dato: "+otroDato)
+    if (nombre=="") {
+      document.getElementById("nombreFormError").style.display="block"
+      validarNombre=false
+    }else{
+      document.getElementById("nombreFormError").style.display="none"
+      validarNombre=true
+    }
+    if (apellido=="") {
+      document.getElementById("apellidoFormError").style.display="block"
+      validarApellido=false
+    }else{
+      document.getElementById("apellidoFormError").style.display="none"
+      validarApellido=true
+    }
+    if (direccion=="") {
+      document.getElementById("direccionFormError").style.display="block"
+      validarDireccion=false
+    }else{
+      document.getElementById("direccionFormError").style.display="none"
+      validarDireccion=true
+    }
+
+    if (edad<18) {
+      document.getElementById("edadFormError").style.display="block"
+      validarEdad=false
+    }else{
+      document.getElementById("edadFormError").style.display="none"
+      validarEdad=true
+    }
+    if(validarNombre&validarApellido&validarDireccion&validarEdad==true){
+      Swal.fire('Datos enviados con exito')
+      
+    }else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Fatan Datos!',
+        footer: '<a href="">Revisa y vuelve a intentar</a>'
+      })
+    }
+    
+}
+function abrirMapa() {
+  Swal.fire({
+    title: 'Chordes!',
+    text: 'Av. Manuel Antonio Matta 470',
+    imageUrl: 'resources/mapaChordes.jpg',
+    imageWidth: 450,
+    imageHeight: 380,
+    imageAlt: 'Custom image',
+  })
 }
